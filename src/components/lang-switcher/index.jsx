67419eb900cc98b61760a5ced750b501fd5f135e
Nikita Kulikov <nikita.kulikov@ewave.com>
2021-02-03 13:39:39 +0300

    styles moved to modules

--- src/components/lang-switcher/index.jsx
+++ src/components/lang-switcher/index.jsx
@@ -3,7 +3,7 @@ import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
 import Maindata from '../../constants'
 
-import './styles.scss'
+import styles from './styles.module.scss'
 
 const Switcher = (props) => {
     const origin = Maindata.url
@@ -21,9 +21,9 @@ const Switcher = (props) => {
     <Helmet htmlAttributes={{ lang: locale }}>
         <meta charSet="utf-8" />
     </Helmet>
-    <div className="language__list">
-        <Link to={props.lang !== 'ru' ? origin + url : origin + postPrefix + url.replace('/ru', '')} className={`language__item${props.lang !== 'ru' ? ' active' : ''}`}>en</Link>
-        <Link to={props.lang === 'ru' ? origin + url : origin + '/ru' + postPrefix + url} className={`language__item${props.lang === 'ru' ? ' active' : ''}`}>ru</Link>
+    <div className={styles.list}>
+        <Link to={props.lang !== 'ru' ? origin + url : origin + postPrefix + url.replace('/ru', '')} className={`${styles.item} ${props.lang !== 'ru' ? styles.active : ''}`}>en</Link>
+        <Link to={props.lang === 'ru' ? origin + url : origin + '/ru' + postPrefix + url} className={`${styles.item} ${props.lang === 'ru' ? styles.active : ''}`}>ru</Link>
     </div>
     </>
 )}
