538d985f2e1045d9353ef389709fcd3b16d6aa07
Nikita Kulikov <nikita.kulikov@ewave.com>
2021-02-03 10:05:18 +0300

    links rework

--- src/pages/index.ru.jsx
+++ src/pages/index.ru.jsx
@@ -60,8 +60,8 @@ const Shibari = () => {
           </div>
           <div className="video-wrapper">
             <video autoPlay loop={true} muted={true} playsInline id="background-video" poster="/shibari-background.webp">
-              <source src="background.webm" type="video/webm" />
-              <source src="background.mp4" type="video/mp4" />
+              <source src="/background.webm" type="video/webm" />
+              <source src="/background.mp4" type="video/mp4" />
             </video>
           </div>
         </div>
