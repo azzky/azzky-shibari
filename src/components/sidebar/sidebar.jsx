52a3814ee1bd8e0efcac0b38b77dd5654fe30bea
Nikita Kulikov <nikita.kulikov@ewave.com>
2021-02-05 09:53:17 +0300

    mastodon link added

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -12,19 +12,24 @@ const Sidebar = (props) => {
 
 const NsfwIcons = (props) => (
     <>
-    <a href={Maindata.socials.instagram_nsfw} rel="noreferrer" target="_blank" aria-label="instagram link">
+    <a href={Maindata.socials.instagram_nsfw} rel="me noreferrer" target="_blank" aria-label="instagram link">
         <svg width="24" height="24" className={props.class}>
         <use href="#instagram"></use>
         </svg>
     </a>
-    <a href={Maindata.socials.telegram} rel="noreferrer" target="_blank" aria-label="telegram link">
+    <a href={Maindata.socials.telegram} rel="me noreferrer" target="_blank" aria-label="telegram link">
         <svg width="24" height="24" className={props.class}>
         <use href="#telegram"></use>
         </svg>
     </a>
-    <a href={Maindata.socials.twitter} rel="noreferrer" target="_blank" aria-label="twitter link">
+    <a href={Maindata.socials.twitter} rel="me noreferrer" target="_blank" aria-label="twitter link">
         <svg width="24" height="24" className={props.class}>
         <use href="#twitter"></use>
+        </svg>
+    </a>
+    <a rel="me noreferrer" href="https://kinbaku.club/@azzky" target="_blank" aria-label="mastodon link">
+        <svg width="24" height="24" className={props.class}>
+        <use href="#mastodon"></use>
         </svg>
     </a>
     </>
