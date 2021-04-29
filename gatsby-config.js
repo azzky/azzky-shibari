3859b61d8496c12b655b05a8a78a25737163876c
Nikita Kulikov <nikita.kulikov@ewave.com>
2021-04-29 17:43:35 +0300

    preload updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-preconnect',
       options: {
-        domains: ['*.ctfassets.net'],
+        domains: ['https://images.ctfassets.net'],
       },
     },
     `gatsby-plugin-offline`,
